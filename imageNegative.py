b4389740d1a401bbe64404532c0cb57ecb2ff800
NGW334 <ngw334@gmail.com>
2022-10-02 06:09:12 +0530

    Image index out of bound error fixed

--- imageNegative.py
+++ imageNegative.py
@@ -2,14 +2,14 @@ from PIL import Image
 
 img = Image.open(r"C:\Users\ngw33\OneDrive\Desktop\ProfilePicturePhoto.jpg")
 
-for x in range(img.height):
-    for y in range(img.width):
-        a,b,c = img.getpixel((x,y))
+for h in range(img.height):
+    for w in range(img.width):
+        a,b,c = img.getpixel((w,h))
 
         a = 255 - a
         b = 255 - b
         c = 255 - c
         d = (a, b, c)
-        img.putpixel((x,y),d)
+        img.putpixel((w,h),d)
 
 img.show()
